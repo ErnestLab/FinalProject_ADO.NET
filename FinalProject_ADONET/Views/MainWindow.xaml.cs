a4c81acc34bca5580bf4b3f00cfb07082ede2a90
Ivon-cpp <65172913+Ivon-cpp@users.noreply.github.com>
2021-05-12 22:00:42 +0300

    Сделан макет главного окна

--- FinalProject_ADONET/Views/MainWindow.xaml.cs
+++ FinalProject_ADONET/Views/MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace FinalProject_ADONET
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
